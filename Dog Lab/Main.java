433c3d3eb0f3dd189ba8dd2a075afee4e5113518
naivy.dominguez24 <naivy.dominguez24@d155.org>
2024-02-07 11:28:40 -0600

    new labrador

--- Dog Lab/Main.java
+++ Dog Lab/Main.java
@@ -11,15 +11,18 @@ public class Main
     public static void main(String[] args)
     {
         Dog[] dogs = new Dog[3];
-	Dog dog = new Labrador("Spike","brown");
+	Dog dog = new Dog("Spike");
 	System.out.println(dog.getName() + " says " + dog.bark());
+
+    Dog dogL = new Labrador("Tim", "brown");
+    System.out.println(dog.getName()+" says "+ dog.bark());
 
 
     Dog dogY = new Yorkshire("Puca",3);
-    System.out.println(dogY.getName() + " "+ dogY.bark());
+    System.out.println(dogY.getName() + " says "+ dogY.bark());
 
 
-        //
+
         (Yorkshire)(dogY).waddle();
         (Labrador)(dog).waddle();
 	
